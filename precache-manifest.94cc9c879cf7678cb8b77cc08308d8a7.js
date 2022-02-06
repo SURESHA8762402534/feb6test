f0c466bc4f927a4454ee2ee4c23506e84933f592
suresh <sureshamahendra.gmail.com>
2022-02-06 19:45:02 +0530

    Updates

---
+++ precache-manifest.94cc9c879cf7678cb8b77cc08308d8a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e1d136027c6bdff295",
+    "url": "/feb6test/static/css/main.f0826fc4.chunk.css"
+  },
+  {
+    "revision": "f5e1d136027c6bdff295",
+    "url": "/feb6test/static/js/main.670f6f21.chunk.js"
+  },
+  {
+    "revision": "81113e0f87d885485a41",
+    "url": "/feb6test/static/js/runtime~main.5e6ffbc0.js"
+  },
+  {
+    "revision": "71d7194958f9367e0e6e",
+    "url": "/feb6test/static/js/2.de6ff777.chunk.js"
+  },
+  {
+    "revision": "b4ae6823da2d4b4a85a4c958b97261fe",
+    "url": "/feb6test/index.html"
+  }
+];
